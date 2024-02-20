c026967cf20161a153bcf444960b095cb8a5d0b1
GermanSC22310223 <143230150+GermanSC22310223@users.noreply.github.com>
2024-02-20 01:32:29 +0000

    Version 0.1

---
+++ include/Mascota.hpp
@@ -0,0 +1,13 @@
+class Mascota
+{
+private:
+    int Felicidad;// Entero el cual es un Objeto
+    int Energia;
+public:
+    Mascota() {}//Constructor solo recibe
+    ~Mascota() {}//Destructor solo cuando la mascota muere
+    void Comer()
+        {
+            this->Energia +=1;//Cada vez que come la mascota se le sube la energia
+        }
+};
--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <Mascota.hpp>
 using namespace std;
+
 
 int main (int argc, char const *argv[])
 {
+    Mascota m1;
+    Mascota m2;
+    Mascota m3;
+    m1.Comer();
+    m2.Comer();
+    m3.Comer();
+    
     cout<<"Hola Mundo"<<"22310223"<<endl;
     return 0;
 }
